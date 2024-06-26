c4b5a48dae139ad7ffc4c314492545c670ee33fe
upaaman <upadhyayaman9685@gmail.com>
2024-06-26 18:50:10 +0530

    chat ws link

--- src/Chat.jsx
+++ src/Chat.jsx
@@ -19,7 +19,7 @@ export default function Chat() {
     connectToWs();
   }, [selectedUserId]);
   function connectToWs() {
-    const ws = new WebSocket('ws://localhost:4040');
+    const ws = new WebSocket('ws://baatcheet-be.onrender.com');
     setWs(ws);
     ws.addEventListener('message', handleMessage);
     ws.addEventListener('close', () => {
